848aba8d56785197ec3fdef3c8f6d203b1863a96
diefero715 <61721255+diefero715@users.noreply.github.com>
2020-05-22 13:08:15 -0500

    Add files via upload

---
+++ app/Http/Controllers/CategoriasController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\categorias;
+use Illuminate\Http\Request;
+
+class CategoriasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\categorias  $categorias
+     * @return \Illuminate\Http\Response
+     */
+    public function show(categorias $categorias)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\categorias  $categorias
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(categorias $categorias)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\categorias  $categorias
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, categorias $categorias)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\categorias  $categorias
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(categorias $categorias)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/ProductoController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Producto;
+use Illuminate\Http\Request;
+
+class ProductoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $data = array();
+        return response()->view("productos/index", $data,200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        $data = array();
+        return response()->view("productos/new", $data, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $name = $request->input("name");
+        $description = $request->input("description");
+        $producto = new Producto;
+        $producto->name = $name;
+        $producto->description = $description;
+        $producto->save();
+        return redirect()->route("productos.index");
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Producto  $producto
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Producto $producto)
+    {
+        //
+        $data = array();
+        return response()->view("productos/show", $data,200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Producto  $producto
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Producto $producto)
+    {
+        //
+        $data = array();
+        return response()->view("productos/edit", $data,200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Producto  $producto
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Producto $producto)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Producto  $producto
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Producto $producto)
+    {
+        //
+    }
+}
